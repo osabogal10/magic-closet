70e56435494d8c98dfb812863318ed86b943dd7c
Orlando Sabogal Rojas <orlando@tyba.com.co>
2025-03-04 03:14:59 -0500

    call server api

--- magic-closet/src/api/services.js
+++ magic-closet/src/api/services.js
@@ -1,5 +1,23 @@
 
-export const selectShoe = (shoeId) => {
+export const selectShoe = async (shoeId) => {
     console.log(`Mandar al back el id ${shoeId}`)
-    // TODO ORLY: mandar al back el id del zapato seleccionado
+    const backendUrl = process.env.BACK_URL ?? 'http://localhost:3001';
+    try {
+      const response = await fetch(`${backendUrl}/move-servo`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ slot: shoeId }),
+      });
+  
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+  
+      const data = await response.json();
+      console.log('Response from server:', data);
+    } catch (error) {
+      console.error('Error:', error);
+    }
   }
